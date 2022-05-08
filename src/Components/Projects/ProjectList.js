dd74ece968823de82fca4d5a37fbe2e16b45340a
Salih Alperen <sal.alperen1@gmail.com>
2022-05-08 22:14:40 +0200

    ADDED new project

--- src/Components/Projects/ProjectList.js
+++ src/Components/Projects/ProjectList.js
@@ -18,6 +18,15 @@ export default [
     techs: ['ContextApi', 'React', 'Auth0', 'Scss'],
   },
   {
+    id: 2,
+    image: require('./todo.png'),
+    live: 'https://react-todo-app-challenge.netlify.app/',
+    repo: 'https://github.com/salperen-jpg/react-todo-app-redux',
+    name: 'Todo App',
+    exp: 'Todo app that can remind you your things that need to be done in some time interval .',
+    techs: ['React', 'Redux', 'Styled Components'],
+  },
+  {
     id: 1,
     image: require('./weather.PNG'),
     live: 'https://weather-around-the-world.netlify.app/',
@@ -25,15 +34,6 @@ export default [
     name: 'Wheather App',
     exp: 'A Weather app that can allow you to display a present weather of specific location .',
     techs: ['React', 'ContextApi', 'Moment.js'],
-  },
-  {
-    id: 2,
-    image: require('./todo.png'),
-    live: 'https://react-todo-app-challenge.netlify.app/',
-    repo: 'https://github.com/salperen-jpg/react-frontendmentor-todoapp-challenge',
-    name: 'Todo App',
-    exp: 'Todo app that can remind you your things that need to be done in some time interval .',
-    techs: ['React', 'ContextApi', 'Reducer'],
   },
 
   {
